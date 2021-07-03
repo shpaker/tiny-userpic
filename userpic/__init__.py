f844278ddc807d460ae157ce4234d2e4852e72a3
shpaker <shpaker@gmail.com>
2021-07-03 17:02:59 +0300

    initial

--- userpic/__init__.py
+++ userpic/__init__.py
@@ -1,10 +1,6 @@
-from userpic.__version__ import __description__, __title__, __version__
 from userpic.userpic import DataFormats, make_userpic
 
 __all__ = (
-    "__description__",
-    "__title__",
-    "__version__",
     "DataFormats",
     "make_userpic",
 )
