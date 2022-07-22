6cbc58a55c9571a08567d9d02085394bb91bf3fa
Aleksandr Shpak <shpaker@users.noreply.github.com>
2022-07-22 22:41:34 +0300

    Remove cairo from required dependencies and replace typer by click (#3)
    remove cairo from required dependencies and replace typer by click
    
    Co-authored-by: A.Shpak <aleksander.shpak.als@gmail.com>

--- tests/test_svg.py
+++ tests/test_svg.py
@@ -1,20 +1,7 @@
-from userpic.blocks import generate_data
-from userpic.svg import make_svg_cell, make_svg_data
+from userpic.svg import _make_svg_cell, make_svg_data
 
-
-def test_make_svg_cell() -> None:
-    cell = make_svg_cell(1, 2, 5, 4, "red")
-    assert cell == '<rect x="9" y="14" width="5" height="5" style="fill:red" />'
-
-
-def test_make_svg_data() -> None:
-    data = [
-        [True, False, True],
-        [False, True, False],
-        [True, False, True],
-    ]
-    svg = make_svg_data(data, 2, 1, "red", "blue")
-    expected_svg = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+_EXPECTED_SVG = """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
     "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg  version="1.1"
@@ -25,5 +12,21 @@ def test_make_svg_data() -> None:
 <rect x="3" y="3" width="2" height="2" style="fill:blue" />
 <rect x="1" y="5" width="2" height="2" style="fill:blue" />
 <rect x="5" y="5" width="2" height="2" style="fill:blue" />
-</svg>'''
-    assert svg.decode().strip() == expected_svg.strip()
+</svg>
+"""
+
+
+def test_make_svg_cell() -> None:
+    cell = _make_svg_cell(1, 2, 5, 4, "red")
+    assert cell == '<rect x="9" y="14" width="5" height="5" style="fill:red" />'
+
+
+def test_make_svg_data() -> None:
+    data = [
+        [True, False, True],
+        [False, True, False],
+        [True, False, True],
+    ]
+    svg = make_svg_data(data, 2, 1, "red", "blue")
+
+    assert svg.decode().strip() == _EXPECTED_SVG.strip()
--- userpic/svg.py
+++ userpic/svg.py
@@ -1,6 +1,6 @@
 from typing import List
 
-SVG_WRAPPER = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+_SVG_WRAPPER = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
     "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg  version="1.1"
@@ -8,19 +8,19 @@ SVG_WRAPPER = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 {rects}
 </svg>
 """
-SVG_RECT_TAG = (
+_SVG_RECT_TAG = (
     '<rect x="{x}" y="{y}" width="{width}" height="{height}" style="fill:{color}" />'
 )
 
 
-def make_svg_cell(
+def _make_svg_cell(
     x: int,
     y: int,
     size: int,
     offset: int,
     color: str,
 ) -> str:
-    return SVG_RECT_TAG.format(
+    return _SVG_RECT_TAG.format(
         x=x * size + offset,
         y=y * size + offset,
         width=size,
@@ -37,7 +37,7 @@ def make_svg_data(
     second_color: str,
 ) -> bytes:
     box_size = len(data) * cell_size + 2 * offset
-    box = make_svg_cell(
+    box = _make_svg_cell(
         x=0,
         y=0,
         offset=0,
@@ -49,7 +49,7 @@ def make_svg_data(
         for x, cell in enumerate(row):
             if not cell:
                 continue
-            rect = make_svg_cell(
+            rect = _make_svg_cell(
                 x=x,
                 y=y,
                 offset=offset,
@@ -57,7 +57,7 @@ def make_svg_data(
                 color=second_color,
             )
             rects.append(rect)
-    svg_xml = SVG_WRAPPER.format(
+    svg_xml = _SVG_WRAPPER.format(
         rects="\n".join(rects),
         width=box_size,
         height=box_size,
